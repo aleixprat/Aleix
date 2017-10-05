59a23f0bea429e1aa5f2307d28209b953a8c9e8c
Aleix <Aleix@DESKTOP-5URUF4M>
2017-10-05 20:41:30 +0200

    commit1 funcio peix

--- php_class_aleix.php
+++ php_class_aleix.php
@@ -3,6 +3,9 @@ class php_class_aleix {
    function __construct() {
        echo "En el constructor BaseClass\n";
    }
+   function peix() {
+       echo "aleix peix"
+   
 }
 
 // En el constructor BaseClass
