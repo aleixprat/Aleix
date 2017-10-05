69a4ce833ef99e402dad2ec34990e349f529626f
Aleix <Aleix@DESKTOP-5URUF4M>
2017-10-05 20:28:08 +0200

    commit1

---
+++ php_class_aleix.php
@@ -0,0 +1,12 @@
+<?php
+class php_class_aleix {
+   function __construct() {
+       echo "En el constructor BaseClass\n";
+   }
+}
+
+// En el constructor BaseClass
+$obj = new php_class_aleix();
+
+
+?>
